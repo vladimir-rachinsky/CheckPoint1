b553a99d652df718869d5f7d5b4c231d2d77078a
vladimir-rachinsky <vladimir.rachinsky@gmail.com>
2015-05-16 15:44:24 +0300

    begin

---
+++ CheckPoint1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckPoint1
+{
+    public class AutoItem
+    {
+        public int Cost { get; set; } // Цена автомобиля
+        public short Speed { get; set; } // Скоорость автомобиля
+        public byte FuelConsuption { get; set; } // Расход топлива
+        public CarType CarType { get; set; } //Тип автомобиля
+        public EngineType EngineType { get; set; } // Тип двигателя
+    }
+    public enum CarType // Тип автомобилей
+    {
+        Sedan,
+        Miniwan,
+        Limusin,
+
+    }
+    public enum EngineType // Тип двигателя
+    {
+        Benzine,
+        Diesiel,
+        Gas
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
